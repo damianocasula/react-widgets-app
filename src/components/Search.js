95994bf845b468fc4a9c1bec2253567dc8b5a46c
Damiano <95046135+damianoc-linear@users.noreply.github.com>
2022-05-14 14:24:06 +0200

    feat: text input with useState hook

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const Search = props => {
-  return <h1>Search</h1>
+  const [term, setTerm] = useState('')
+
+  return (
+    <div>
+      <div className='ui form'>
+        <div className='field'>
+          <label>Search</label>
+          <input
+            className='input'
+            value={term}
+            onChange={e => setTerm(e.target.value)}
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Search
