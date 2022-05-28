3d6f4128f6430d4ffa45841da079cad6a83c50bf
damianocasula <95046135+damianocasula@users.noreply.github.com>
2022-05-28 15:03:09 +0200

    feat: react 18 rendering

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+// Use new React rendering API createRoot
+const rootElement = document.getElementById('root')
+ReactDOM.createRoot(rootElement).render(<App />)
